7cf79bf393d7ef059c59047d941a93997512846a
Luis Henrique <gavirate.henrique@gmail.com>
2021-06-29 03:23:59 -0300

    feat: controllers and actions

---
+++ DevFitness/DevFitness.API/Controllers/MealsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users/5/meals
+    [Route("api/users/{userId}/meals")]
+    public class MealsController : ControllerBase
+    {
+        // api/users/5/meals - GET
+        [HttpGet]
+        public IActionResult GetAll(int userId)
+        {
+            return Ok();
+        }
+
+        // api/users/5/meals/12 - GET
+        [HttpGet("{mealId}")]
+        public IActionResult Get(int userId, int mealId)
+        {
+            return Ok();
+        }
+
+        // api/users/5/meals
+        [HttpPost]
+        public IActionResult Post(int userId, [FromBody] CreateMealInputModel inputModel)
+        {
+            return Ok();
+        }
+
+        // api/users/5/meals/12 - PUT
+        [HttpPut("{mealId}")]
+        public IActionResult Put(int userId, int mealId, [FromBody] UpdateMealInputModel inputModel)
+        {
+            return NoContent();
+        }
+
+        // api/users/5/meals/12 - DELETE
+        [HttpDelete]
+        public IActionResult Delete(int userId, int mealId)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFitness/DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        // api/users/5 - GET
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //return NotFound();
+
+            return Ok();
+        }
+
+        // api/users - POST
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+        {
+            //return BadRequest();
+
+            return CreatedAtAction(nameof(Get), new { id = 10 }, inputModel);
+        }
+
+        // api/users/5 - PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
+        {
+            // return NotFound();
+            // return BadRequest();
+
+            return NoContent();
+        }
+    }
+}
